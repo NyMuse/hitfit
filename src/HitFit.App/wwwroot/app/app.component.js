e5361c3ed5e99f5813217462fd96f4faf1f3ef36
jayMax <maksim.garnets@gmail.com>
2017-03-12 13:32:25 +0200

    Fix app.

--- src/HitFit.App/wwwroot/app/app.component.js
+++ src/HitFit.App/wwwroot/app/app.component.js
@@ -29,7 +29,7 @@ AppComponent = __decorate([
     <h1>My First Angular 2 App</h1>
     <ul>
     <li *ngFor="let person of persons">
-    <strong></strong><br>
+    <strong>person.name</strong><br>
     from: <br>
     date of birth: 
     </li>
